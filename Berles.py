0a93226271b1f9ba64ca7edac59895436ab6adc6
VTomi80 <167123053+VTomi80@users.noreply.github.com>
2024-10-15 17:34:54 +0200

    Update Berles.py

--- Berles.py
+++ Berles.py
@@ -1,21 +1,16 @@
 from Autok import Auto
 from Berlo import Berlo
+from datetime import date
+
 
 class Berles:
-    def __init__(self, auto:Auto, berlo:Berlo, kezdet, napok:int):
+    def __init__(self, auto: Auto, berlo: Berlo, kezdet, napok: int):
         self.auto = auto
         self.berlo = berlo
         self.kezdet = kezdet
         self.napok = napok
         self.ar = auto.dij * napok
-        self.auto.elerheto = False
+        self.vege = date(int(kezdet[0: 4]), int(kezdet[5: 7]), int(kezdet[8:]) + napok)
 
     def __str__(self):
-        return f"Bérlő {self.berlo.nev}, Rendszám: {self.auto.rendszam}, Bérlés kezdete: {self.kezdet}, Bérlés időtartama: {self.napok} nap"
-
-    def lemond(self):
-        self.auto.elerheto = True
-
-
-
-
+        return f"Bérlő {self.berlo.nev}, Rendszám: {self.auto.rendszam}, Bérlés kezdete: {self.kezdet}, Bérlés vége: {self.vege}, Bérlés időtartama: {self.napok} nap"
